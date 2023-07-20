605f31a008f1a9a0506fe38a92d8d431a413dec2
Emre Üstün <emreust26@gmail.com>
2023-07-20 23:33:51 +0300

    side bar ve header altyapısı oluşturuldu.

--- WebPages/ba-card-app/src/App.js
+++ WebPages/ba-card-app/src/App.js
@@ -4,14 +4,17 @@ import {
 import Sayfa1 from './pages';
 import MlakatSayfasi from './pages/MulakatSayfa';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
       <BrowserRouter>
+      <Header />
+      <Sidebar/>
         <Routes>
           {/* <Route path="/" element={<Home />} /> */}
           {/* { <Route path="/" element={<Sayfa1 />} /> } */}
-          { <Route path="/" element={<Header/>} /> }
+          {/* { <Route path="/" element={<Header/>} /> }  */}
           {/* <Route path="/login" element={<Login />} /> */}
           {/* <Route path="/register" element={<Register />} /> */}
         </Routes>
--- WebPages/ba-card-app/src/components/Header.js
+++ WebPages/ba-card-app/src/components/Header.js
@@ -6,10 +6,13 @@ import Buyutec from "../icons/Buyutec.svg";
 const Header = () => {
 
     return (
-        <div style={{}}>
-            <div className="frame-group" >
+        <div>
+            <div className="frame-group" style={{
+                position: 'absolute',
+                left: '50%',
+                transform: 'translate(-50%, -20%)'
+            }}   >
                 <div className="group-parent">
-                    {/* <img className="frame-child" alt="" src="../icons/Group-106.svg" /> */}
                     <img src={BilgeAdamLogo} alt="SVG Resmi" />
                     <div className="div">|</div>
                     <div className="renci">Öğrenci</div>
