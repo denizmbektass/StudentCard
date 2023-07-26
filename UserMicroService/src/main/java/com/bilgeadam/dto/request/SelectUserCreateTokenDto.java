1eadd662a42f105f1b3155489a1c500da45f90ea
Ahmet <ahmettolayy@gmail.com>
2023-07-27 02:18:00 +0300

    userId token a çevirildi ,JwtTokenManager yazıldı

--- UserMicroService/src/main/java/com/bilgeadam/dto/request/SelectUserCreateTokenDto.java
+++ UserMicroService/src/main/java/com/bilgeadam/dto/request/SelectUserCreateTokenDto.java
@@ -1,17 +1,21 @@
 package com.bilgeadam.dto.request;
 
+import com.bilgeadam.repository.entity.EStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class SearchUserRequestDto {
-    private  String name;
-    private  String surname;
-    private  String email;
-    private  String phoneNumber;
+public class SelectUserCreateTokenDto {
+
+    private String studentId;
+    private List<String> role;
+    private EStatus status;
+
 }
