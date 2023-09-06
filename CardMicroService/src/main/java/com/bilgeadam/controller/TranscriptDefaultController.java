b73a2fd5c232f268cfc4f44b2104dc2311f7a562
SeçilÇakır <117979909+secil1313@users.noreply.github.com>
2023-09-06 14:31:50 +0300

    transcript default düzenleme

---
+++ CardMicroService/src/main/java/com/bilgeadam/controller/TranscriptDefaultController.java
@@ -0,0 +1,31 @@
+package com.bilgeadam.controller;
+
+import com.bilgeadam.dto.request.TranscriptDefaultRequestDto;
+import com.bilgeadam.dto.response.TranscriptDefaultResponseDto;
+import com.bilgeadam.repository.view.VwGroupName;
+import com.bilgeadam.service.TranskriptDefaultService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.bilgeadam.constants.ApiUrls.*;
+
+@RestController
+@RequestMapping(TRANSCRIPT_DEFAULT)
+@RequiredArgsConstructor
+public class TranscriptDefaultController {
+    private final TranskriptDefaultService transkriptDefaultService;
+    @CrossOrigin("*")
+    @GetMapping("/taking-group-names")
+    public ResponseEntity<List<VwGroupName>> takingGroupNames(){
+        return ResponseEntity.ok(transkriptDefaultService.takingGroupNames());
+    }
+    @CrossOrigin("*")
+    @PostMapping("/creating-transcript-datas")
+    public ResponseEntity<Boolean> creatingTranscriptDatas(@RequestBody TranscriptDefaultRequestDto transcriptDefaultRequestDto){
+        return ResponseEntity.ok(transkriptDefaultService.creatingTranscriptDatas(transcriptDefaultRequestDto));
+    }
+
+    }
