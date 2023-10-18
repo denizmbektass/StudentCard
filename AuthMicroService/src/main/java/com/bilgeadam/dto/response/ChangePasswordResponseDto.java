e68c767f853a83fd3d2fa3310ba02fd7cfb461a8
HivdaAydogan <hivdaydgn@gmail.com>
2023-10-18 19:30:35 +0300

    Reset&Change password regex düzenlendi.

--- AuthMicroService/src/main/java/com/bilgeadam/dto/response/ChangePasswordResponseDto.java
+++ AuthMicroService/src/main/java/com/bilgeadam/dto/response/ChangePasswordResponseDto.java
@@ -5,11 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Pattern;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ChangePasswordResponseDto {
 String userId;
+@Pattern( message = "Password requires to be with at least eight characters." +
+        " It must includes at least one lower, one upper, one special character and a number",
+        regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=*!.,?])(?=\\S+$).{8,}$")
 String newPassword;
 }
