263d18d720b2083ec7d71c38f5d9d67d03b8c3d8
mehmetJava8Hibernate <mehmetarif96@gmail.com>
2023-10-26 22:55:37 +0300

    Task638 Algoritma card backend metodları yazıldı

---
+++ CardMicroService/src/main/java/com/bilgeadam/dto/response/AlgorithmResponseDto.java
@@ -0,0 +1,16 @@
+package com.bilgeadam.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlgorithmResponseDto {
+    private Double firstScore;
+    private Double secondScore;
+    private Double finalScore;
+}
--- CardMicroService/src/main/java/com/bilgeadam/exceptions/ErrorType.java
+++ CardMicroService/src/main/java/com/bilgeadam/exceptions/ErrorType.java
@@ -64,7 +64,8 @@ public enum ErrorType {
     BEHAVIOR_NUMBER_RANGE(9023,"Davranış puanı '0' ile '100' arasında olmak zorundadır", HttpStatus.BAD_REQUEST),
     BEHAVIOR_POINT_EMPTY(9024,"Davranış puanı boş bırakılamaz", HttpStatus.BAD_REQUEST),
     BEHAVIOR_NOT_FOUND(9025, "Proje bulunamadı", HttpStatus.BAD_REQUEST),
-    TOTAL_PERCENTAGE(9026,"Girilen puanlama yüzdelerinin toplamı tam 100'e eşit olmalıdır",HttpStatus.BAD_REQUEST);
+    TOTAL_PERCENTAGE(9026,"Girilen puanlama yüzdelerinin toplamı tam 100'e eşit olmalıdır",HttpStatus.BAD_REQUEST),
+    ALGORITHM_NOT_FOUND(9027, "Algoritma puanı bulunamadı", HttpStatus.BAD_REQUEST);
 
     private int code;
     private String message;
