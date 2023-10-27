c67a62f4a1759311c877f5cda9620d98c2053464
muhammetkrkrt <karakurt3656@gmail.com>
2023-10-27 12:50:28 +0300

    task670 add GetGraduationProjectResponse dto use in method

--- CardMicroService/src/main/java/com/bilgeadam/mapper/IGraduationProjectMapper.java
+++ CardMicroService/src/main/java/com/bilgeadam/mapper/IGraduationProjectMapper.java
@@ -1,6 +1,7 @@
 package com.bilgeadam.mapper;
 
 import com.bilgeadam.dto.request.CreateGraduationProjectRequestDto;
+import com.bilgeadam.dto.response.GetGraduationProjectResponseDto;
 import com.bilgeadam.repository.entity.GraduationProject;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
@@ -10,5 +11,6 @@ import org.mapstruct.factory.Mappers;
 public interface IGraduationProjectMapper {
     IGraduationProjectMapper INSTANCE = Mappers.getMapper(IGraduationProjectMapper.class);
     GraduationProject toGraduationProject(final CreateGraduationProjectRequestDto dto);
+    GetGraduationProjectResponseDto toGraduationProject(final GraduationProject graduationProject);
 
 }
