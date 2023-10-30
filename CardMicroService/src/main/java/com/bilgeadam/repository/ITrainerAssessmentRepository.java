a82d63f3dbd1650a81d6b4b725f5461cdc27145d
mcanm <mcanmavili95@gmail.com>
2023-10-30 13:27:42 +0300

    changes

--- CardMicroService/src/main/java/com/bilgeadam/repository/ITrainerAssessmentRepository.java
+++ CardMicroService/src/main/java/com/bilgeadam/repository/ITrainerAssessmentRepository.java
@@ -1,15 +1,14 @@
 package com.bilgeadam.repository;
 
-import com.bilgeadam.repository.entity.Assignment;
 import com.bilgeadam.repository.entity.TrainerAssessment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
+
 @Repository
 public interface ITrainerAssessmentRepository extends MongoRepository<TrainerAssessment,String> {
 
-    Optional<TrainerAssessment> findOptionalByTrainerAssessmentId(String id);
+    // Optional<TrainerAssessment> findOptionalByTrainerAssessmentId(String id);
     List<TrainerAssessment> findAllByStudentId(String studentId);
 }
