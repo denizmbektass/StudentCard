e455dc36d496117b69571c309c87064c42c397eb
muhammetkrkrt <karakurt3656@gmail.com>
2023-10-31 03:58:04 +0300

    task693 revision all TrainerAssessmentCoefficients

--- CardMicroService/src/main/java/com/bilgeadam/mapper/ITrainerAssessmentCoefficientsMapper.java
+++ CardMicroService/src/main/java/com/bilgeadam/mapper/ITrainerAssessmentCoefficientsMapper.java
@@ -1,15 +1,18 @@
 package com.bilgeadam.mapper;
 
-import com.bilgeadam.dto.request.UpdateTrainerAssessmentCoefficientsRequestDto;
+import com.bilgeadam.dto.request.SaveTrainerAssessmentRequestDto;
+import com.bilgeadam.dto.response.GetGraduationProjectResponseDto;
+import com.bilgeadam.dto.response.GetTrainerAssessmentCoefficientsResponseDto;
+import com.bilgeadam.repository.entity.GraduationProject;
 import com.bilgeadam.repository.entity.TrainerAssessmentCoefficients;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ITrainerAssessmentCoefficientsMapper {
 
     ITrainerAssessmentCoefficientsMapper INSTANCE = Mappers.getMapper(ITrainerAssessmentCoefficientsMapper.class);
-    TrainerAssessmentCoefficients toUpdateTrainerAssessmentCoefficients(final UpdateTrainerAssessmentCoefficientsRequestDto dto);
-    TrainerAssessmentCoefficients toDeleteTrainerAssessmentCoefficients(final TrainerAssessmentCoefficients trainerAssessmentCoefficients);
+    TrainerAssessmentCoefficients toCreateTrainerAssessmentCoefficients(final SaveTrainerAssessmentRequestDto dto);
+    GetTrainerAssessmentCoefficientsResponseDto toTrainerAssessmentCoefficients (final TrainerAssessmentCoefficients trainerAssessmentCoefficients);
 }
