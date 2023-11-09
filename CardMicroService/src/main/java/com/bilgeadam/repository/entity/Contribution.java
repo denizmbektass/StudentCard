54bc46fa874651d067841dd5a4443e38d12c2913
mcanm <mcanmavili95@gmail.com>
2023-11-09 05:10:41 +0300

    Entity sınıfı ve Repository interface'i yazıldı.

---
+++ CardMicroService/src/main/java/com/bilgeadam/repository/entity/Contribution.java
@@ -0,0 +1,24 @@
+package com.bilgeadam.repository.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Document
+public class Contribution {
+
+    @Id
+    private String contributionId;
+    private double incorrectCodeOrDisplayMessageNote;
+    private double documentationForBacklogNote;
+    private double researchNote;
+    private double intraTeamTrainingNote;
+    private double totalScoreContribution;
+}
