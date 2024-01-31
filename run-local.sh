34c5a87b2bf1dd623df037abcaa80d082de80086
Emin Arıkan <emin.arikan@bilgeadam.com>
2024-01-31 21:05:36 +0300

    change jrxml font

--- run-local.sh
+++ run-local.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-gnome-terminal --tab --title="ApiGatewayService" -- ./gradlew :ApiGatewayService:bootRun -Dspring.profiles.active=local
-gnome-terminal --tab --title="AuthMicroService" -- ./gradlew :AuthMicroService:bootRun -Dspring.profiles.active=local
-gnome-terminal --tab --title="CardMicroService" -- ./gradlew :CardMicroService:bootRun -Dspring.profiles.active=local
-gnome-terminal --tab --title="UserMicroService" -- ./gradlew :UserMicroService:bootRun -Dspring.profiles.active=local
-gnome-terminal --tab --title="MailService" -- ./gradlew :MailService:bootRun -Dspring.profiles.active=local
+# Function to execute a command in a tab
+run_command_in_tab() {
+    gnome-terminal --tab --title="$1" -- bash -c "$2; echo Press Enter to close this tab; read"
+}
+
+# Run commands in tabs
+run_command_in_tab "ApiGatewayService" "./gradlew --warning-mode=all --stacktrace :ApiGatewayService:bootRun -Dspring.profiles.active=local"
+run_command_in_tab "AuthMicroService" "./gradlew --warning-mode=all --stacktrace :AuthMicroService:bootRun -Dspring.profiles.active=local"
+run_command_in_tab "CardMicroService" "./gradlew --warning-mode=all --stacktrace :CardMicroService:bootRun -Dspring.profiles.active=local"
+run_command_in_tab "UserMicroService" "./gradlew --warning-mode=all --stacktrace :UserMicroService:bootRun -Dspring.profiles.active=local"
+run_command_in_tab "MailService" "./gradlew --warning-mode=all --stacktrace :MailService:bootRun -Dspring.profiles.active=local"
