138fe5b2e896379fbab56e08d85e3c9de2237008
Emin Arıkan <emin.arikan@bilgeadam.com>
2024-01-31 16:17:25 +0300

    update run-local.sh

--- run-local.sh
+++ run-local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gnome-terminal -- ./gradlew :ApiGatewayService:bootRun -Dspring.profiles.active=local
-gnome-terminal -- ./gradlew :AuthMicroService:bootRun -Dspring.profiles.active=local
-gnome-terminal -- ./gradlew :CardMicroService:bootRun -Dspring.profiles.active=local
-gnome-terminal -- ./gradlew :UserMicroService:bootRun -Dspring.profiles.active=local
-gnome-terminal -- ./gradlew :MailService:bootRun -Dspring.profiles.active=local
+gnome-terminal --tab --title="ApiGatewayService" -- ./gradlew :ApiGatewayService:bootRun -Dspring.profiles.active=local
+gnome-terminal --tab --title="AuthMicroService" -- ./gradlew :AuthMicroService:bootRun -Dspring.profiles.active=local
+gnome-terminal --tab --title="CardMicroService" -- ./gradlew :CardMicroService:bootRun -Dspring.profiles.active=local
+gnome-terminal --tab --title="UserMicroService" -- ./gradlew :UserMicroService:bootRun -Dspring.profiles.active=local
+gnome-terminal --tab --title="MailService" -- ./gradlew :MailService:bootRun -Dspring.profiles.active=local
